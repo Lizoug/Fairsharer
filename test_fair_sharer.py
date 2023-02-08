266383bca0126a3fb8adc77dcaaab7794f30ec77
Liza Kaladjian <lizakalaijiann@gmail.com>
2023-02-08 16:06:34 +0100

    updated test function

--- test_fair_sharer.py
+++ test_fair_sharer.py
@@ -10,5 +10,11 @@ def test_fair_sharer():
     assert fair_sharer.fair_sharer([0, 100, 200, 300], 3) == [50, 200, 300, 250]
     assert fair_sharer.fair_sharer([0, 1000, 1000, 0], 1) == [100, 900, 900, 100]
     assert fair_sharer.fair_sharer([0, 1000, 1000, 1000], 2) == [200, 800, 1000, 800]
+    
+    # Additional test cases
+    input_data = [1, 2, 3, 4]
+    expected_output = [1, 2, 3, 4]
+    num_iterations = 0
+    assert fair_sharer(input_data, num_iterations) == expected_output
 
 test_fair_sharer()
