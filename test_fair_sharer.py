ee1243316e0f7d1c862ce21935af56f114a4ca9e
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-02-10 21:33:26 +0100

    Update test_fair_sharer.py

--- test_fair_sharer.py
+++ test_fair_sharer.py
@@ -1,20 +1,16 @@
-import fair_sharer
+from fair_sharer import fair_sharer
 
 # import could not worked as i wished to
 
 def test_fair_sharer():
-    assert fair_sharer.fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
-    assert fair_sharer.fair_sharer([0, 0, 0, 1000], 2) == [180, 0, 180, 640]
-    assert fair_sharer.fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
-    assert fair_sharer.fair_sharer([0, 0, 0, 1000], 2) == [190, 0, 190, 720]
-    assert fair_sharer.fair_sharer([0, 100, 200, 300], 3) == [50, 200, 300, 250]
-    assert fair_sharer.fair_sharer([0, 1000, 1000, 0], 1) == [100, 900, 900, 100]
-    assert fair_sharer.fair_sharer([0, 1000, 1000, 1000], 2) == [200, 800, 1000, 800]
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
+    assert fair_sharer([0, 0, 0, 1000], 2) == [180, 0, 180, 640]
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
+    assert fair_sharer([0, 100, 200, 300], 3) == [54, 125, 204, 217]
+    assert fair_sharer([0, 1000, 1000, 0], 1) == [100, 800, 1100, 0]
+    assert fair_sharer([0, 1000, 1000, 1000], 2) == [100, 910, 880, 1110]
     
-    # Additional test cases
-    input_data = [1, 2, 3, 4]
-    expected_output = [1, 2, 3, 4]
-    num_iterations = 0
-    assert fair_sharer(input_data, num_iterations) == expected_output
+    result = fair_sharer([0, 1000, 800, 0], 1)
+    assert max(result) == 900
 
 test_fair_sharer()
